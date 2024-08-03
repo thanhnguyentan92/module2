7bf679a5ea61014d107d1b3882d9dc7bbf2b6911
thanhnguyen <thanhnguyentan92@gmail.com>
2024-08-02 20:32:05 -0600

    Update Main.java

--- module2/src/main/java/org/example/Main.java
+++ module2/src/main/java/org/example/Main.java
@@ -225,7 +225,7 @@ public class Main {
     public static void main(String[] args) throws ParseException {
         QuanLyBenhAn quanLy = new QuanLyBenhAn();
         quanLy.Menu(quanLy);
-        quanLy.hienThiBenhAn();
+        
 
     }
 }
